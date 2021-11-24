a24a4e3c52d406da25bcd542a94de1db7ed29412
Razerzhang <m17307483121@163.com>
2021-11-25 01:50:06 +0800

    updata

--- lru/lru.go
+++ lru/lru.go
@@ -1,1 +1,32 @@
 package lru
+
+import "container/list"
+
+type Cache struct {
+
+
+	ll *list.List
+	cache map[interface{}]*list.Element
+}
+
+type Key interface {}
+
+type entry struct {
+	key Key
+	value interface{}
+}
+
+func Init()*Cache  {
+	return &Cache{
+		ll: list.New(),
+		cache:  make(map[interface{}]*list.Element),
+	}
+}
+
+//Add add a value to the Cache
+func (c *Cache)Add()  {
+	if c.cache == nil{
+		c.cache = make(map[interface{}]*list.Element)
+		c.ll = list.New()
+	}
+}
